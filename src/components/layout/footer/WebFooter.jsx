a7d227d923e0046b59bc4c59ac45ffb1950db7f6
Hasan Raza <hr1938082@gmail.com>
2021-10-29 17:19:23 +0500

    update footer

--- src/components/layout/footer/WebFooter.jsx
+++ src/components/layout/footer/WebFooter.jsx
@@ -3,17 +3,32 @@ import { NavLink } from 'react-router-dom'
 
 const WebFooter = () => {
     return (
-        <nav className="navbar navbar-expand navbar-light bg-light fixed-bottom">
+        <footer className="navbar navbar-expand navbar-light">
             <div className="container-fluid">
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink className="nav-link" activeClassName="active" aria-current="page" to="/">Home</NavLink>
+                            <NavLink className="nav-link" activeClassName="active" to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/policy">Terms of Service</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/mission">Mission</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/pricing">Pricing</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/affiliates">Affiliates</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
                         </li>
                     </ul>
                 </div>
             </div>
-        </nav>
+        </footer>
     )
 }
 
