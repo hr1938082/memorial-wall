baeab3a96f5d8ff68be6c47c3cc98b98030ca254
Hasan Raza <hr1938082@gmail.com>
2021-10-29 16:40:39 +0500

    update navbar

--- src/components/layout/navbar/WebNavbar.jsx
+++ src/components/layout/navbar/WebNavbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import { MdSearch, MdMic, MdOutlineSettings, MdLogout } from 'react-icons/md';
+import { BsQuestionLg } from 'react-icons/bs'
+import { GiCandleLight } from 'react-icons/gi'
+import { Tooltip, Menu, MenuItem, Avatar, Divider, ListItemIcon } from '@mui/material';
+import user from '../../../images/userDummy.png'
 const WebNavbar = () => {
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <nav className="navbar navbar-light navbar-expand-lg bg-light fixed-top">
+        <nav className="navbar navbar-light navbar-expand-lg fixed-top">
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to="#">Memorial Wall</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
@@ -17,10 +30,109 @@ const WebNavbar = () => {
                     <div className="offcanvas-body">
                         <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                                <NavLink className="nav-link" activeClassName="active" to="/">
+                                    Advance Search
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="#">Link</NavLink>
+                                <div className="search">
+                                    <MdSearch />
+                                    <input type="text" name="search" className="" />
+                                    <MdMic />
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <GiCandleLight
+                                    style={{
+                                        color: 'white',
+                                        fontSize: '20px'
+                                    }}
+                                />
+                            </li>
+                            <li className="nav-item">
+                                <select class="form-select" aria-label=".form-select-sm example">
+                                    <option selected>Wall</option>
+                                    <option value="1">Community</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/fund">
+                                    Fund
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/memorialize">
+                                    Memorialize
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <Tooltip title="setting">
+                                    <NavLink className="nav-link" activeClassName="active" to="/setting">
+                                        <MdOutlineSettings />
+                                    </NavLink>
+                                </Tooltip>
+                            </li>
+                            <li className="nav-item">
+                                <Tooltip title="FAQ">
+                                    <NavLink className="nav-link" activeClassName="active" to="/faq">
+                                        <BsQuestionLg />
+                                    </NavLink>
+                                </Tooltip>
+                            </li>
+                            <li className="nav-item">
+                                <Tooltip title="Profile">
+                                    <div className="profile" onClick={handleClick}>
+                                        <img src={user} alt="User Profile" />
+                                    </div>
+                                </Tooltip>
+                                <Menu
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                    onClick={handleClose}
+                                    PaperProps={{
+                                        elevation: 0,
+                                        sx: {
+                                            overflow: 'visible',
+                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                            mt: 1.5,
+                                            '& .MuiAvatar-root': {
+                                                width: 32,
+                                                height: 32,
+                                                ml: -0.5,
+                                                mr: 1,
+                                            },
+                                            '&:before': {
+                                                content: '""',
+                                                display: 'block',
+                                                position: 'absolute',
+                                                top: 0,
+                                                right: 14,
+                                                width: 10,
+                                                height: 10,
+                                                bgcolor: 'background.paper',
+                                                transform: 'translateY(-50%) rotate(45deg)',
+                                                zIndex: 0,
+                                            },
+                                        },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem>
+                                        <Avatar >
+                                            <img src={user} alt="User Profile" style={{ height: '100%' }} /></Avatar> Profile
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem>
+                                        <ListItemIcon>
+                                            <MdLogout fontSize="small" />
+                                        </ListItemIcon>
+                                        Logout
+                                    </MenuItem>
+                                </Menu>
                             </li>
                         </ul>
                     </div>
